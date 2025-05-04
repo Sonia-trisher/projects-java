2ef83ac2fb4eecf0a9daf069f64b0fb9e96d926e
sonia-trisher <soniatrisher86@gmail.com>
2025-05-04 23:20:29 +0200

    merged

---
+++ Generic mergeArrays/src/GenericArrayUtility.java
@@ -0,0 +1,14 @@
+import java.lang.reflect.Array;
+
+public class GenericArrayUtility {
+    public static <T> T[] mergeArrays(T[] a, T[] b) {
+    T[] result = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length + b.length);
+
+    System.arraycopy(a, 0, result, 0, a.length);
+
+    System.arraycopy(b, 0, result, a.length, b.length);
+
+    return result;
+
+}
+}
---
+++ Generic mergeArrays/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Integer[] intArray1 = {1, 2, 3};
+        Integer[] intArray2 = {4, 5, 6};
+
+        Integer[] mergedInts = GenericArrayUtility.mergeArrays(intArray1, intArray2);
+        System.out.print("Merged Integer Array: ");
+        for (Integer num : mergedInts) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        String[] strArray1 = {"Hello", "World"};
+        String[] strArray2 = {"Java", "Generics"};
+
+        String[] mergedStrings = GenericArrayUtility.mergeArrays(strArray1, strArray2);
+        System.out.println("Merged String Array:");
+        for (String word : mergedStrings) {
+            System.out.print(word + " ");
+        }
+        System.out.println();
+    }
+}
