80d8d6558cbfd59643984c8dc7c611a0e2067a4b
sonia-trisher <soniatrisher86@gmail.com>
2025-05-06 00:01:25 +0200

    arraycopy

--- Generic mergeArrays/src/GenericArrayUtility.java
+++ Generic mergeArrays/src/GenericArrayUtility.java
@@ -1,14 +1,10 @@
 import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class GenericArrayUtility {
     public static <T> T[] mergeArrays(T[] a, T[] b) {
-    T[] result = (T[]) Array.newInstance(a.getClass().getComponentType(), a.length + b.length);
-
-    System.arraycopy(a, 0, result, 0, a.length);
-
-    System.arraycopy(b, 0, result, a.length, b.length);
-
-    return result;
-
+        T[] result = Arrays.copyOf(a,a.length + b.length);
+        System.arraycopy(b,0,result,a.length,b.length);
+        return result;
 }
 }
--- Generic mergeArrays/src/Main.java
+++ Generic mergeArrays/src/Main.java
@@ -4,11 +4,13 @@ public class Main {
         Integer[] intArray1 = {1, 2, 3};
         Integer[] intArray2 = {4, 5, 6};
 
-        Integer[] mergedInts = GenericArrayUtility.mergeArrays(intArray1, intArray2);
+        Integer[]mergedInts =GenericArrayUtility.mergeArrays(intArray1,intArray2);
         System.out.print("Merged Integer Array: ");
-        for (Integer num : mergedInts) {
+        for(Integer num : mergedInts){
             System.out.print(num + " ");
         }
+
+
         System.out.println();
 
         String[] strArray1 = {"Hello", "World"};
