635371816ce59ca2574ea39e7455ca9a54952669
sonia-trisher <soniatrisher86@gmail.com>
2025-05-07 14:58:01 +0200

    constructor

--- MethodReference/src/MethodReference.java
+++ MethodReference/src/MethodReference.java
@@ -31,17 +31,16 @@ public class MethodReference {
 //        lambda expression
 //        team.sort((t1,t2) -> t1. compareTo(t2));
         System.out.println("Sorted team: "+team);
-
-        List<String> theBestStudents = Arrays.asList("Sonia", "Channelle", "Ingabire", "Queen", "Muhimpundu", "Anne", "Florence", "Mutuyimana", "Gaju");
-//
-        List<Person> theBest = theBestStudents.stream()
-                .map(st -> new Person(st))
+        List<String> theBestSudents =Arrays.asList("Sonia", "Channelle", "Ingabire", "Queen", "Muhimpundu", "Anne", "Florence", "Mutuyimana", "Gaju");
+        List<Person> theBest = theBestSudents.stream()
+                .map(st ->new Person(st))
                 .toList();
-//
-//        List<Person> theBest = theBestStudents.stream()
-//                .map(Person::new)
-//                .toList();
-//        // Syntax: ClassName::new
         System.out.println(theBest);
+
+////        List<Person> theBest = theBestStudents.stream()
+////                .map(Person::new)
+////                .toList();
+////        // Syntax: ClassName::new
+//        System.out.println(theBest);
     }
 }
